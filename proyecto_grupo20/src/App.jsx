e59f96ee1db65b19699180921f4d37b7c98cbfc7
Juan Pablo <Juanpablo.rodriguezhe@gmail.com>
2021-10-01 17:43:35 -0500

    agregamos la fuente roboto de materia ui

--- proyecto_grupo20/src/App.jsx
+++ proyecto_grupo20/src/App.jsx
@@ -1,1 +1,17 @@
+import React from "react";
+import Button from "@material-ui/core/Button"
+import { ThemeProvider } from "@material-ui/styles";
+import Navbar from "./Components/Navbar"
 
+
+function App(){
+    return(
+        <div>
+            <Button>
+            Hola Mundo
+            </Button>
+        </div>
+    );
+}
+
+export default App;
